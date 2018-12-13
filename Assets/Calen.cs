d9de426a515f04e7e2ce0084278183f98b1ad388
CaelanRP <caelanrp@gmail.com>
2018-12-13 13:50:22 -0800

    basic movement backend

--- Assets/Calen.cs
+++ Assets/Calen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Calen : MonoBehaviour {
 	Camera cam;
-	public float walkSpeed, jumpForce;
+	public float walkAccel, walkDecel, walkMaxSpeed, jumpForce;
 	public static Calen instance;
 	void Awake(){
 		instance = this;
@@ -20,4 +20,42 @@ public class Calen : MonoBehaviour {
 		//transform.rotation = Camera.main.transform.rotation;
 		//transform.eulerAngles = new Vector3(-5f, transform.eulerAngles.y, transform.eulerAngles.z);
 	}
+
+	void FixedUpdate(){
+	    HandleInputFixed();
+	}
+
+
+	void HandleInputFixed(){
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(xMove, 0, zMove);
+
+        if (xMove == 0)
+        {
+            //decelerate x
+        }
+
+        if (zMove == 0)
+        {
+            //decelerate z
+        }
+
+
+        if (xMove != 0 || zMove != 0)
+        {
+            Walk(moveVector);
+        }
+        
+    }
+
+    void Walk(Vector3 moveVector)
+    {
+        moveVector = Vector3.ClampMagnitude(moveVector, 1);
+
+        moveVector *= walkAccel;
+
+        moveVector = Vector3.ClampMagnitude(moveVector, walkMaxSpeed);
+    }
 }
